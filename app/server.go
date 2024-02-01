00f87bfe1da85536a3dc9bb4b728433a5ca97b72
Thien Nguyen <thien.chi.nguyen@vn.centralretail.com>
2024-02-01 13:07:55 +0700

    3rd stage

--- app/server.go
+++ app/server.go
@@ -17,12 +17,18 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
+
+	conn, err := l.Accept()
+	if err != nil {
+		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
 	for {
-		conn, err := l.Accept()
+		_, err := conn.Read([]byte{})
 		if err != nil {
-			fmt.Println("Error accepting connection: ", err.Error())
-			os.Exit(1)
+			fmt.Println("Error reading from connection")
 		}
 		conn.Write([]byte("+PONG\r\n"))
 	}
+
 }
