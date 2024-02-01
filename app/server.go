c0d5621545d513f11b726e63b63484c309e52e17
Thien Nguyen <thien.chi.nguyen@vn.centralretail.com>
2024-02-01 17:20:06 +0700

    stag 4

--- app/server.go
+++ app/server.go
@@ -19,13 +19,17 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		go handleConnection(conn)
 	}
+}
 
+func handleConnection(conn net.Conn) {
 	scanner := bufio.NewScanner(conn)
 
 	for scanner.Scan() {
